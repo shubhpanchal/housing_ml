f149fd9b1434250771f185e3f9071475f5ccb77f
shubhpanchal <shubhpanchal412@gmail.com>
2023-02-09 14:55:32 +0530

    Defined Configuration for Data Ingestion and Training Pipeline

---
+++ housing/util/util.py
@@ -0,0 +1,13 @@
+import yaml
+from housing.exceptions import HousingException
+import os, sys
+
+def read_yaml_file(file_path :str)->dict:
+    """
+    Reads a YAML file returns the contents as dictionary
+    """
+    try:
+        with open(file_path, 'rb') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise HousingException(e, sys) from e
