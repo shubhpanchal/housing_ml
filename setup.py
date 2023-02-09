ad84a2bb39e5929c2263b41297d9cba8ac5463cb
shubhpanchal <shubhpanchal412@gmail.com>
2023-02-09 13:15:06 +0530

    updated requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from typing import List
 
 PROJECT_NAME = "housing-predictor"
 VERSION = "0.0.1"
 AUTHOR = "Shubham Panchal"
 DESCRIPTION = "This project aims to predict housing prices"
-PACKAGES = ["housing"]
 REQUIREMENT_FILE_NAME = "requirements.txt"
 
 def get_requirements_list()-> List[str]:
@@ -16,13 +15,13 @@ def get_requirements_list()-> List[str]:
     returns the list which contains the name of libraries mentioned in requirements.txt
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
     
 setup(
     name = PROJECT_NAME,
     version=VERSION,
     author  = AUTHOR,
     description= DESCRIPTION,
-    packages=PACKAGES,
+    packages=find_packages(),
     install_requires=get_requirements_list()
 )
