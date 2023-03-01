62b9b3284f8d734a7c011c97a0037a5fd73f52a3
shubhpanchal <shubhpanchal412@gmail.com>
2023-03-01 21:11:57 +0530

    Data Validation Configuration and Pipeline Defined

--- demo.py
+++ demo.py
@@ -1,19 +1,16 @@
 from housing.pipeline.pipeline import Pipeline
 from housing.exception import HousingException
-from housing.config.configuration import Configuration
 from housing.logger import logging
-import os, sys
-
+from housing.config.configuration import Configuartion
 def main():
     try:
-        pipeline = Pipeline()
-        pipeline.run_pipeline()
-        # data_validation_config = Configuration().get_data_validation_config()
-        # print(data_validation_config)
+        # pipeline = Pipeline()
+        # pipeline.run_pipeline()
+        data_validation_config = Configuartion().get_data_transformation_config()
+        print(data_validation_config)
     except Exception as e:
         logging.error(f"{e}")
         print(e)
 
-
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -3,20 +3,18 @@ import sys
 
 class HousingException(Exception):
     
-    def __init__(self, error_message:Exception, error_detail:sys):
+    def __init__(self, error_message:Exception,error_detail:sys):
         super().__init__(error_message)
-        self.error_message = HousingException.get_detailed_error_message(error_message=error_message,
-                                                                     error_detail=error_detail
-                                                                     )
-    
+        self.error_message=HousingException.get_detailed_error_message(error_message=error_message,
+                                                                       error_detail=error_detail
+                                                                        )
+
 
     @staticmethod
-    def get_detailed_error_message(error_message:Exception, error_detail:sys)-> str:
-        """This function gets the detailed error message
-
-        Args:
-            error_message (Exception): Exception Object
-            error_detail (sys): Object of sys module
+    def get_detailed_error_message(error_message:Exception,error_detail:sys)->str:
+        """
+        error_message: Exception object
+        error_detail: object of sys module
         """
         _,_ ,exec_tb = error_detail.exc_info()
         exception_block_line_number = exec_tb.tb_frame.f_lineno
@@ -29,9 +27,11 @@ class HousingException(Exception):
         error message: [{error_message}]
         """
         return error_message
-    
+
     def __str__(self):
         return self.error_message
-    
-    def __repr__(self)->str:
+
+
+    def __repr__(self) -> str:
         return HousingException.__name__.str()
+
